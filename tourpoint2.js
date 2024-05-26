9e806c4325bc22e8d383543d4eb82ce7af10997c
WalidKarn <158018780+WalidKarn@users.noreply.github.com>
2024-05-27 02:06:16 +0700

    Add files via upload

---
+++ tourpoint2.js
@@ -0,0 +1,171 @@
+tourpoint =
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "เซ็นทรัลพลาซา ลาดพร้าว",
+        "type": "ห้าง",
+        "class": "A"
+      },
+      "geometry": {
+        "coordinates": [
+          100.56054722835802,
+          13.816641208965933
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "เซ็นทรัลพลาซา แกรนด์ พระราม 9",
+        "type": "ห้าง",
+        "class": "A"
+      },
+      "geometry": {
+        "coordinates": [
+          100.56603150000075,
+          13.758370289482244
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "เซ็นทรัลพลาซา พระราม 3",
+        "type": "ห้าง",
+        "class": "A"
+      },
+      "geometry": {
+        "coordinates": [
+          100.53769772883413,
+          13.69767678815198
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "เซ็นทรัลพลาซา บางนา",
+        "type": "ห้าง",
+        "class": "A"
+      },
+      "geometry": {
+        "coordinates": [
+          100.63437549999867,
+          13.66875493717913
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "เซ็นทรัลพลาซา ปิ่นเกล้า",
+        "type": "ห้าง",
+        "class": "A"
+      },
+      "geometry": {
+        "coordinates": [
+          100.4762570000014,
+          13.77799837143138
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "เซ็นทรัลพลาซา เวสต์เกต",
+        "type": "ห้าง",
+        "class": "A"
+      },
+      "geometry": {
+        "coordinates": [
+          100.41154099999886,
+          13.877282792173801
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "เซ็นทรัลพลาซา แจ้งวัฒนะ",
+        "type": "ห้าง",
+        "class": "A"
+      },
+      "geometry": {
+        "coordinates": [
+          100.5280216250012,
+          13.903850582182557
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "เซ็นทรัล ชิดลม",
+        "type": "ห้าง",
+        "class": "A"
+      },
+      "geometry": {
+        "coordinates": [
+          100.54445122876558,
+          13.74442886767315
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "เซ็นทรัล เอ็มบาสซี",
+        "type": "ห้าง",
+        "class": "A"
+      },
+      "geometry": {
+        "coordinates": [
+          100.54643572880252,
+          13.743774632420852
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "เซ็นทรัลเวิลด์",
+        "type": "ห้าง",
+        "class": "A"
+      },
+      "geometry": {
+        "coordinates": [
+          100.53905053967122,
+          13.746512210763314
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "type": "ห้าง",
+        "name": "เซ็นทรัล เฟสติวัล อิสวิว ",
+        "class": "A"
+      },
+      "geometry": {
+        "coordinates": [
+          100.61465827116263,
+          13.804389209473172
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
